ff855866b9a36e13fc437e3008a3951fad42fd4e
opilane <opilane@HP-CZC6297B92>
2021-10-28 11:35:25 +0300

    proverka

--- _1.py
+++ _1.py
@@ -1,2 +1,8 @@
-print("Hello")
-print("hi")
+print("Камень ножницы бумага ")
+from random import *
+while type(a) !=int:
+    try:
+        a=input("Введите камень-1 ножницы-2 бумага-3: ")
+    except TypeError:
+            print("Viga")
+b=randint(1,3)
