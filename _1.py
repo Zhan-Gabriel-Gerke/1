ae6dd11fde9d11aad88846d4342484f8844e60cb
opilane <opilane@HP-CZC6297B92>
2021-10-29 11:31:52 +0300

    kamen noznitsi bumaga

--- _1.py
+++ _1.py
@@ -1,8 +1,97 @@
 print("Камень ножницы бумага ")
 from random import *
-while type(a) !=int:
-    try:
-        a=input("Введите камень-1 ножницы-2 бумага-3: ")
-    except TypeError:
-            print("Viga")
-b=randint(1,3)
+if 1==int(input("Хотите ли вы поиграть 0 or 1")):
+    g=int(input("Вы хотите поиграть с роботом или с другом  или чтобы робот с роботом 1 or 2 or 3"))
+    if g==1:
+        win=0
+        lose=0
+        m=0
+        while True:
+            a=int(input("Выберите 1-камень 2-ножницы 3-бумага"))
+            b=randint(1,3)
+            if a==b:
+                print("ничья")
+            if a==1 and b==2:
+                print("Ты победил")
+                win+=1
+            if a==2 and b==3:
+                print("Ты победил")
+                win+=1
+            if a==3 and b==1:
+                print("Ты победил")
+                win+=1
+            if a==b:
+                    print("ничья")
+            if b==1 and a==2:
+                print("Вы проиграли")
+                lose+=1
+            if b==2 and a==1:
+                print("Вы проиграли")
+                lose+=1
+            if b==3 and a==1:
+                print("Вы проиграли")
+                lose+=1
+        if win==3:
+            print("Ты победил")
+        if lose==3:
+            print("Ты проиграл")
+        print(win)
+        print(lose)
+    elif g==2:
+        win=0
+        lose=0
+        print("вы выбрали поиграть с другом")
+        while True:
+            a=int(input("Выберите 1-камень 2-ножницы 3-бумага"))
+            b=int(input("Выберите 1-камень 2-ножницы 3-бумага"))
+            if a==b:
+                print("ничья")
+            if a==1 and b==2:
+                print("победил игрок номер 1")
+                win+=1
+            if a==2 and b==3:
+                print("победил игром номер 1")
+                win+=1
+            if a==3 and b==1:
+                print("победил игрок номер 1")
+                win+=1
+            if b==1 and a==2:
+                print("победил игрок номер 2")
+                lose+=1
+            if b==2 and a==1:
+                print("победил игрок номер 2")
+                lose+=1
+            if b==3 and a==1:
+                print("победил игрок номер 2")
+                lose+=1
+    elif g==3:
+        from keyboard import *
+        from random import *
+        print("kivi, käärid, paber")
+        m=3
+        while m not in [1,2]:
+            try:
+                m=int(input("kellega mängime?\n1-botid \n2-robotiga"))
+            except:
+                ValueError
+        v1=["kivi, käärid, paber"]
+        v2=["kivi, käärid, paber"]
+        if m==1:
+            while True:
+                print("kas nängime? esc - välja, enter - mängime")
+                if read_key()=='esc':
+                    break
+                elif read_key()=='enter':
+                    p1=choice(v1)
+                    print("esimene boot:",p1)
+                    p2=choice(v2)
+                    print("teine bot:",p2)
+                if p1==p2:
+                    print("viik")
+                elif p1==v1[0] and p2==v2[1] or p1==v2[0] or p1==v1[1] and p2==v2[2]:
+                    print("võitis, 1")
+                else:
+                    print("võitis, 2")
+        elif m==2:
+            while 1:
+                pass
