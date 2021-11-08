e968ee82327811182ee204e3f5718c46250e505f
zhan-gabriel <93312041+euro631@users.noreply.github.com>
2021-11-08 19:11:42 +0300

    Update _1.py

--- _1.py
+++ _1.py
@@ -66,32 +66,20 @@ if 1==int(input("Хотите ли вы поиграть 0 or 1")):
                 lose+=1
     elif g==3:
         from keyboard import *
-        from random import *
-        print("kivi, käärid, paber")
-        m=3
-        while m not in [1,2]:
-            try:
-                m=int(input("kellega mängime?\n1-botid \n2-robotiga"))
-            except:
-                ValueError
-        v1=["kivi, käärid, paber"]
-        v2=["kivi, käärid, paber"]
-        if m==1:
-            while True:
-                print("kas nängime? esc - välja, enter - mängime")
-                if read_key()=='esc':
-                    break
-                elif read_key()=='enter':
-                    p1=choice(v1)
-                    print("esimene boot:",p1)
-                    p2=choice(v2)
-                    print("teine bot:",p2)
-                if p1==p2:
-                    print("viik")
-                elif p1==v1[0] and p2==v2[1] or p1==v2[0] or p1==v1[1] and p2==v2[2]:
-                    print("võitis, 1")
-                else:
-                    print("võitis, 2")
-        elif m==2:
-            while 1:
-                pass
+        v1=['Камень','Ножницы','Бумага'] 
+        v2=['Камень','Ножницы','Бумага'] 
+        while True: 
+            print('Играем? esc= выходим, enter= играем') 
+            if read_key()=='esc': 
+                break 
+            elif read_key()=='enter': 
+                p1=choice(v1) 
+                print('Первый Бот: ',p1) 
+                p2=choice(v2) 
+                print('Второй Бот: ',p2) 
+                if p1==p2: 
+                    print('Ничья') 
+                elif p1==v1[0] and p2==v2[1] or p1==v1[2] and p2==v2[0] or p1==v1[1] and p2==v2[2]: 
+                    print('Выйграл 1') 
+                else: 
+                    xˇxprint("Выйграл 2")
